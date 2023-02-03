105a4713e96e2961ebc3f72989d3a599ba72096e
Matteo Piccini <gnappo.1@live.it>
2023-02-03 11:56:21 -0800

    Solution with additional code

--- lib/seed.py
+++ lib/seed.py
@@ -14,3 +14,18 @@ if __name__ == '__main__':
     engine = create_engine('sqlite:///seed_db.db')
     Session = sessionmaker(bind=engine)
     session = Session()
+    # empty db first
+    with session.begin():
+        session.query(Game).delete()
+    # instantiate 2 game objects
+        game1 = Game(title="Crash Bandicot", genre = "fantasy", platform = "nintendo", price = 12)
+        game2 = Game(title="Tomb Raider", genre = "fantasy", platform = "nintendo", price = 12)
+        # add them all to the session
+        session.bulk_save_objects([game1, game2], return_defaults=True)
+    # session.add_all([game1, game2])
+
+    # commit the session
+    # session.commit()
+    print(session.query(Game).count())
+    print(session.query(Game).first())
+    print(session.query(Game).order_by(Game.id.desc()).first())
